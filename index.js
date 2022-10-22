d97f8a00caf87bce387f15f8faf03c789dedf42d
arraywego <elara5@uci.edu>
2022-10-21 17:11:22 -0700

    updated index and mytest files

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(
     })
 )
 app.get('/', (request, response) => {
-    response.json({
-        info: 'Node.js, Express, and Postgres API'
-    })
+    response.sendFile(__dirname + '/index.html')
 })
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
